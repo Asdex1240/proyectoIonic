7420288ca08d68b3a611867bc24b8e241aef0267
ValeC46 <ivaleria.cordero@gmail.com>
2022-04-18 21:58:53 -0500

    lamapra enciende

--- pia/src/app/tab1/tab1.page.ts
+++ pia/src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Flashlight } from '@awesome-cordova-plugins/flashlight/ngx';
+import { Geolocation } from "@capacitor/geolocation";
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
-  constructor(private router: Router) {}
+  currentCenter: { lat: any; lng: any; };
+
+  constructor(private router: Router, private flashlight: Flashlight) {}
+
   fov(frove){
     if(frove== 'frutas'){
       console.log('FRUTAS');
@@ -15,7 +22,21 @@ export class Tab1Page {
       this.router.navigate(['tabs/tab1/verduras']);
       console.log('VERDURAS');
     }else if(frove=='linterna'){
-      console.log('LINTERNA');
-    }
+      this.currentCenter = {lat: 25.72569943459976, lng: -100.31498633730602};
+      if(this.getCurrentPosition()){
+        this.flashlight.switchOn();
+        console.log('ON');
+      }else{
+        console.log('No esta en la ubicacion.');
+      }
+    }      
+  }
+
+  async getCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    this.currentCenter = {
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude
+    };
   }
 }
